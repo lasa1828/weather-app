42ff4a2355a09eddde9f10bc6816f0f6db046f87
lasa1828 <91841708+lasa1828@users.noreply.github.com>
2021-11-22 16:39:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,29 +48,27 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+function showWeather(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 
-function showCurrentTemperature(response) {
   let currentTemperature = Math.round(response.data.main.temp);
   let temperature = document.querySelector("#current-temperature");
   temperature.innerHTML = `${currentTemperature}`;
   celsiusTemperature = response.data.main.temp;
-}
-function showHumidity(response) {
-  let currentHumidity = Math.round(response.data.main.humidity);
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${currentHumidity}%`;
-}
-function showWind(response) {
-  let currentWind = Math.round(response.data.wind.speed);
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = `Wind: ${currentWind}km/h`;
-}
-function showWeather(response) {
+
   let currentWeather = response.data.weather[0].description;
   let weather = document.querySelector("#weather-description");
   weather.innerHTML = `${currentWeather}`;
-}
-function showIcon(response) {
+
+  let currentWind = Math.round(response.data.wind.speed);
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${currentWind}km/h`;
+
+  let currentHumidity = Math.round(response.data.main.humidity);
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${currentHumidity}%`;
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -78,15 +76,13 @@ function showIcon(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function search(city) {
   let apiKey = "c5e1f4b02577647cb3da3a656eabc850";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCurrentTemperature);
-  axios.get(apiUrl).then(showHumidity);
-  axios.get(apiUrl).then(showWind);
   axios.get(apiUrl).then(showWeather);
-  axios.get(apiUrl).then(showIcon);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-search");
